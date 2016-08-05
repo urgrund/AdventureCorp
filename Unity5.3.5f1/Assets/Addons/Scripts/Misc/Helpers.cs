439de9610410f83584c9595c1491aa13a1b61338
urgrund <mattbthe1st@gmail.com>
2016-08-05 10:33:44 +1000

    removed help lab as it had 1 funciton,  moved to Helpers

--- Unity5.3.5f1/Assets/Addons/Scripts/Misc/Helpers.cs
+++ Unity5.3.5f1/Assets/Addons/Scripts/Misc/Helpers.cs
@@ -249,4 +249,24 @@ public static class Helpers
         NavMeshHit hit;
         return !NavMesh.Raycast(p + Vector3.up, p - Vector3.down * 3, out hit, NavMesh.AllAreas);
     }
+
+
+
+	public static void DrawMarker(Vector3 position, float size, Color color, float duration, bool depthTest)
+	{
+		Vector3 line1PosA = position + Vector3.up * size * 0.5f;
+		Vector3 line1PosB = position - Vector3.up * size * 0.5f;
+
+		Vector3 line2PosA = position + Vector3.right * size * 0.5f;
+		Vector3 line2PosB = position - Vector3.right * size * 0.5f;
+
+		Vector3 line3PosA = position + Vector3.forward * size * 0.5f;
+		Vector3 line3PosB = position - Vector3.forward * size * 0.5f;
+
+		Debug.DrawLine(line1PosA, line1PosB, color, duration, depthTest);
+		Debug.DrawLine(line2PosA, line2PosB, color, duration, depthTest);
+		Debug.DrawLine(line3PosA, line3PosB, color, duration, depthTest);
+	}
+
+
 }
